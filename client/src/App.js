c19820c5277cf4047b43ed1c1787d6374744887b
pokuakn <abena.saulka@columbia.edu>
2025-04-20 18:12:21 -0500

    Added "error' text

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <div>
-                        <h1>Fib Calculator</h1>
+                        <h1>Fib Calculator: Error</h1>
                         <Link to="/">Home</Link><br />
                         <Link to="/otherpage">Other Page</Link>
                     </div>
